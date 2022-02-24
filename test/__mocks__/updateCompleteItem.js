10bbf64baa873e19646d5a0517ba3b9a98b77292
Estebanmual <esteban.munozal@gmail.com>
2022-02-24 17:56:32 -0600

    On branch tests2 	modified:   test/__mocks__/updateCompleteItem.js Change module export to make work the UpdateCompleted function

--- test/__mocks__/updateCompleteItem.js
+++ test/__mocks__/updateCompleteItem.js
@@ -1,18 +1,19 @@
 function updateCompleted(tasks, id, checked) {
-  tasks.forEach((element) => {
-    if (checked === true) {
+  if (checked === true) {
+    tasks.forEach((element) => {
       if (id === element.index) {
         element.completed = true;
       }
-    } else if (checked === false) {
-      tasks.forEach((element) => {
-        if (id === element.index) {
-          element.completed = false;
-        }
-      });
-    }
-  });
+    });
+  } else if (checked === false) {
+    tasks.forEach((element) => {
+      if (id === element.index) {
+        element.completed = false;
+      }
+    });
+  }
+
   return tasks;
 }
 
-module.exports = updateCompleted();
+module.exports = updateCompleted;
